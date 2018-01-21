b95fa1c5d320eeb7c8dcf70cf36ffdd9a65ececd
Daniil Pyšniak <dpk125@users.noreply.github.com>
2018-01-21 13:20:14 +0200

    Add links to the homepage

---
+++ client/components/MenuItem/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+export const MenuItem = ({ backgroundImage, name, path }) => {
+  return (
+    <div className="col-sm-4">
+      <Link to={path}>
+        <div className="menu-item"
+             style={{ backgroundImage: `url('${backgroundImage}')` }}>
+          <span>{name}</span>
+        </div>
+      </Link>
+    </div>
+  )
+};
+
+MenuItem.propTypes = {
+  backgroundImage: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  path: PropTypes.string.isRequired
+};
--- client/pages/Home/index.js
+++ client/pages/Home/index.js
@@ -1,67 +1,55 @@
 import React from 'react';
-import history from '../../core/history';
+import { MenuItem } from '../../components/MenuItem';
+import { Link } from 'react-router-dom';
 
 export const Home = () => (
   <div className="row">
     <div className="col-sm-5">
       <div className="subtitle">Welcome to</div>
       <div className="title">Movies repository</div>
-      <div className="btn" onClick={() => history.push('/genres')}>
+      <Link to="/genres" className="btn">
         Select a genre
-      </div>
+      </Link>
     </div>
 
     <div className="col-sm-7">
       <div className="menu">
         <div className="row">
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/A30ZqEoDbchvE7mCZcSp6TEwB1Q.jpg')` }}>
-              <span>All</span>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/paT9Ovofjj30JvsDRyhFfjgUEfn.jpg')` }}>
-              <span>Search</span>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/vsjBeMPZtyB7yNsYY56XYxifaQZ.jpg')` }}>
-              <span>Genres</span>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/vjZRXcAUH33SHsQeMScwsZcA5uk.jpg')` }}>
-              <span>Most popular</span>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/nbIrDhOtUpdD9HKDBRy02a8VhpV.jpg')` }}>
-              <span>Top rated</span>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/8fDHXLHYTOLR064YIWxxcVVl2qz.jpg')` }}>
-              <span>Upcomming</span>
-            </div>
-          </div>
-
-          <div className="col-sm-4">
-            <div className="menu-item"
-                 style={{ backgroundImage: `url('https://image.tmdb.org/t/p/w342/xSxlmtLz2NYtO07N4WexY1y53pl.jpg')` }}>
-              <span>Wishlist</span>
-            </div>
-          </div>
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/A30ZqEoDbchvE7mCZcSp6TEwB1Q.jpg"
+            name="All"
+            path="/"
+          />
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/paT9Ovofjj30JvsDRyhFfjgUEfn.jpg"
+            name="Search"
+            path="/search"
+          />
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/vsjBeMPZtyB7yNsYY56XYxifaQZ.jpg"
+            name="Genres"
+            path="/genres"
+          />
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/vjZRXcAUH33SHsQeMScwsZcA5uk.jpg"
+            name="Most popular"
+            path="/most-popular"
+          />
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/nbIrDhOtUpdD9HKDBRy02a8VhpV.jpg"
+            name="Top rated"
+            path="/top-rated"
+          />
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/8fDHXLHYTOLR064YIWxxcVVl2qz.jpg"
+            name="Upcoming"
+            path="/upcoming"
+          />
+          <MenuItem
+            backgroundImage="https://image.tmdb.org/t/p/w342/xSxlmtLz2NYtO07N4WexY1y53pl.jpg"
+            name="Wishlist"
+            path="/wishlist"
+          />
         </div>
       </div>
     </div>

7c29dcabc6b8231ca0e5e6d84f6c1bd146f50115
Daniil Pyšniak <dpk125@users.noreply.github.com>
2018-01-21 14:05:09 +0200

    Handle empty movie posters

--- client/components/Movie/index.js
+++ client/components/Movie/index.js
@@ -2,16 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { getPosterUrl } from '../../core/helpers/imageUrlResolver';
 
-export const Movie = ({ title, rating, poster }) => (
-  <div className="movie-list-item"
-       style={{ backgroundImage: `url(${getPosterUrl(poster, 'w640')})` }}>
-    <span className="movie-list-item__rating">{rating}</span>
-    <span className="movie-list-item__name">{title}</span>
-  </div>
-);
+export const Movie = ({ title, rating, poster }) => {
+  const style = {};
+  if (poster) {
+    style.backgroundImage = `url(${getPosterUrl(poster, 'w640')})`;
+  } else {
+    style.backgroundColor = '#271625';
+  }
+
+  return (
+    <div className="movie-list-item" style={style}>
+      <span className="movie-list-item__rating">{rating || '?'}</span>
+      <span className={'movie-list-item__name' + (poster ? '' : ' movie-list-item__name--visible')} >
+        {title}
+      </span>
+    </div>
+  );
+}
 
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-  poster: PropTypes.string.isRequired,
+  rating: PropTypes.number,
+  poster: PropTypes.string,
 };
